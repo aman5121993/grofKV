74fecba1ade4083472d095dba2866bbd82d2c0bd
amit handa <amit.handa@geminisolutions.in>
2019-02-20 02:38:19 +0530

    pub/sub2

--- client/client.py
+++ client/client.py
@@ -1,12 +1,11 @@
 import argparse
+import redis
 import requests
-import asyncio
-import websockets
 
 def menu():
  parser = argparse.ArgumentParser()
  parser.add_argument("mode",help="enter mode get ,post or watch")
- parser.add_argument("key",help="enter key")
+ parser.add_argument("--key",help="enter key")
  parser.add_argument("--value",help="Enter Value")
  args=parser.parse_args()
  
@@ -15,8 +14,7 @@ def menu():
  if args.mode == "post":
   postmeth(args.key,args.value)
  if args.mode == "watch":
-  asyncio.get_event_loop().run_until_complete(hello(args.key))
- 
+  watch() 
 
 def getmeth(key):
  res=requests.get('http://10.10.71.11:5000/getkey/'+key)
@@ -30,15 +28,14 @@ def postmeth(key,val):
  print (r.json())
 
 
-async def hello(key):
-  async with websockets.connect(
-            'ws://10.10.71.11:8765') as websocket:
-        await websocket.send(key)
-        while True:
-         val = await websocket.recv()
-         val = val.decode()
-         print ("Value is :  " + val)
+def watch():
+ r = redis.StrictRedis(host='10.10.71.11', port=6379)
+ p = r.pubsub()
+ p.psubscribe('__keyspace@0__:*')
 
+ for message in p.listen():
+  print ("Change in key: " + message['channel'].decode().split(':',1)[1])
+ 
 
 if __name__ == "__main__":
  menu()
