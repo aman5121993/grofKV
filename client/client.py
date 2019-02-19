d36c484716e1f01155d50cb9d1ece5caf3de0676
amit handa <amit.handa@geminisolutions.in>
2019-02-20 02:44:38 +0530

    pub/sub3

--- client/client.py
+++ client/client.py
@@ -3,39 +3,39 @@ import redis
 import requests
 
 def menu():
- parser = argparse.ArgumentParser()
- parser.add_argument("mode",help="enter mode get ,post or watch")
- parser.add_argument("--key",help="enter key")
- parser.add_argument("--value",help="Enter Value")
- args=parser.parse_args()
+   parser = argparse.ArgumentParser()
+   parser.add_argument("mode",help="enter mode get ,post or watch")
+   parser.add_argument("--key",help="enter key")
+   parser.add_argument("--value",help="Enter Value")
+   args=parser.parse_args()
  
- if args.mode == "get":
-  getmeth(args.key)
- if args.mode == "post":
-  postmeth(args.key,args.value)
- if args.mode == "watch":
-  watch() 
+   if args.mode == "get":
+      getmeth(args.key)
+   if args.mode == "post":
+      postmeth(args.key,args.value)
+   if args.mode == "watch":
+      watch() 
 
 def getmeth(key):
- res=requests.get('http://10.10.71.11:5000/getkey/'+key)
- if res.ok:
-  print (res.json())
+   res=requests.get('http://10.10.71.11:5000/getkey/'+key)
+   if res.ok:
+      print (res.json())
 
 
 def postmeth(key,val):
- payload = {"key": key,"value":val}
- r = requests.post('http://10.10.71.11:5000/postkey',json=payload)
- print (r.json())
+   payload = {"key": key,"value":val}
+   r = requests.post('http://10.10.71.11:5000/postkey',json=payload)
+   print (r.json())
 
 
 def watch():
- r = redis.StrictRedis(host='10.10.71.11', port=6379)
- p = r.pubsub()
- p.psubscribe('__keyspace@0__:*')
+   r = redis.StrictRedis(host='10.10.71.11', port=6379)
+   p = r.pubsub()
+   p.psubscribe('__keyspace@0__:*')
 
- for message in p.listen():
-  print ("Change in key: " + message['channel'].decode().split(':',1)[1])
+   for message in p.listen():
+      print ("Change in key: " + message['channel'].decode().split(':',1)[1])
  
 
 if __name__ == "__main__":
- menu()
+   menu()
--- server/app.py
+++ server/app.py
@@ -11,14 +11,16 @@ db=0)
 @app.route("/postkey", methods=['GET','POST'])
 def postkey():
    if request.method == 'POST':
-    cnt=request.json
-    r.set(cnt['key'],cnt['value'])
-    return jsonify({"response":"OK"})
+      cnt=request.json
+      r.set(cnt['key'],cnt['value'])
+      return jsonify({"response":"OK"})
+
 
 @app.route("/getkey/<key>",methods=['GET'])
 def getkey(key):
- print(key)
- return jsonify({"val":r.get(key)})
+   print(key)
+   return jsonify({"val":r.get(key)})
+
 
 if __name__ == "__main__":
  app.run(debug=True,host='0.0.0.0')
